8def3ec298445be5c48cc898f5f1e71bbbeeb7c5
Leon <60602221+Lenchi30@users.noreply.github.com>
2020-11-26 22:24:03 +0100

    Additions , Functions

---
+++ dist/scriptFunctions.js
@@ -0,0 +1,19 @@
+function switchToSensor(){
+    $("#logPage").show();
+    $("#aboutPage").hide();
+    $("#settingsPage").hide();
+}
+
+function switchToAbout(){
+    $("#logPage").hide();
+    $("#aboutPage").show();
+    $("#settingsPage").hide();
+}
+
+function switchToSettings(){
+    $("#logPage").hide();
+    $("#aboutPage").hide();
+    $("#settingsPage").show();
+}
+
+
